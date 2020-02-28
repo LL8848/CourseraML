e664e0b4d75755706c3142c836749e7f8e6a7394
Lingnan <ustclnlin@gmail.com>
2020-02-28 12:46:37 -0500

    add

---
+++ ex1/data/ex1.m
@@ -0,0 +1,18 @@
+clear;
+clc;
+
+data = load('ex1data1.txt');
+
+x = data(:,1);
+y = data(:,2);
+
+m = length(y);
+
+X = [ones(m,1), data(:,1)];
+theta = zeros(2,1);
+
+iterations = 1500;
+alpha = 0.1;  % hjhjlk
+
+nkj
+ljljl
